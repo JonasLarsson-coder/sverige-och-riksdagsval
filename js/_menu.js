54f020ee80c7910d5e626ca25ccd77002e95fee7
Max Nilsson <Maxstjernbergnilsson@gmail.com>
2025-04-15 10:18:48 +0200

    max meny sida

--- js/_menu.js
+++ js/_menu.js
@@ -3,4 +3,5 @@ createMenu('Sverige och våra senaste två riksdagsval', [
   { name: 'Visa ett år', script: 'one-year.js' },
   { name: 'Jämför två år', script: 'compare-two-years.js' },
   { name: 'Hitta trender', script: 'trends.js' }
+  { name: 'Max testar sin sida', script: 'max-test.js' },
 ]);                             
