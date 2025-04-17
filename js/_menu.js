400e8599fb796b96fb16d6e852ec186f76fa15da
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-04-17 10:58:25 +0200

    update

--- js/_menu.js
+++ js/_menu.js
@@ -4,5 +4,5 @@ createMenu('Sverige och våra senaste två riksdagsval', [
   { name: 'Jämför två år', script: 'compare-two-years.js' },
   { name: 'Hitta trender', script: 'trends.js' },
   { name: 'Max testar sin sida', script: 'max-sida.js' },
-  { name: 'Jonas testar sin sida', script: 'JonasTest.js' },
+  { name: 'Jonas testar sin sida', script: 'Jonas.js' },
 ]);                             
