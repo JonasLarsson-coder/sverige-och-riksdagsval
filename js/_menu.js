74e1e79253c607ffe1ac206c4af310729a0cc0be
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-04-29 19:21:58 +0200

    Löste merge-konflikt i _menu.js – behåller bådas menyval

--- js/_menu.js
+++ js/_menu.js
@@ -1,15 +1,8 @@
 createMenu('Sverige och våra senaste två riksdagsval', [
-<<<<<<< HEAD
-  { name: 'Nytt i version 7', script: 'new-in-v7.js' },
-  { name: 'Visa ett år', script: 'one-year.js' },
-  { name: 'Jämför två år', script: 'compare-two-years.js' },
-  { name: 'Hitta trender', script: 'trends.js' },
   { name: 'Max testar sin sida', script: 'max-sida.js' },
-  { name: 'Jonas testar sin sida', script: 'JonasTest.js' },
   { name: 'Max sammanfattning', script: 'samm.js' },
-]);                             
-=======
   { name: 'Arbetslöshet Vs. Röster', script: 'arbetslöshetOchRöster.js' },
-  { name: '2018 Vs. 2022', script: '2018Vs2022.js' },
-]);                               
->>>>>>> jonasBranch
+  { name: '2018 Vs. 2022', script: '2018Vs2022.js' }
+]);
+
+
