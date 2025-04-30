d92a7635e7406227d4bc4d4029170ffa480b6277
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-04-30 12:55:10 +0200

    ändrade fliknamn

--- js/_menu.js
+++ js/_menu.js
@@ -2,8 +2,8 @@ createMenu('Sverige och våra senaste två riksdagsval', [
   { name: 'Inledning', script: 'inledning.js' },
   { name: 'Utbildning och valresultat', script: 'max-sida.js' },
   { name: 'Sammanfattning utbildning', script: 'samm.js' },
-  { name: 'Arbetslöshet Vs. Röster', script: 'sammanfattningDiagram.js' },
-  { name: '2018 Vs. 2022', script: '2018Vs2022.js' }
+  { name: 'Arbetslöshet och valresultat', script: 'sammanfattningDiagram.js' },
+  { name: '2018 jämfört med 2022', script: '2018Vs2022.js' }
 ]);
 
 
