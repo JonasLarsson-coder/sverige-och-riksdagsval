2cde8f03e5fafd49bcabf499a8ab0e73109e089f
JonasLarsson-coder <Jonas.larsson.data24sto@edu.tucsweden.se>
2025-04-30 09:55:19 +0200

    update

--- js/_menu.js
+++ js/_menu.js
@@ -2,7 +2,7 @@ createMenu('Sverige och våra senaste två riksdagsval', [
   { name: 'Inledning', script: 'inledning.js' },
   { name: 'Utbildning och valresultat', script: 'max-sida.js' },
   { name: 'Sammanfattning utbildning', script: 'samm.js' },
-  { name: 'Arbetslöshet Vs. Röster', script: 'arbetslöshetOchRöster.js' },
+  { name: 'Arbetslöshet Vs. Röster', script: 'sammanfattningDiagram.js' },
   { name: '2018 Vs. 2022', script: '2018Vs2022.js' }
 ]);
 
